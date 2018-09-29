0d00c5b02fbde38431ce1969617721220dd91946
GabrielZanini <39746969+GabrielZanini@users.noreply.github.com>
2018-09-28 22:45:21 -0300

    Bugs

---
+++ TestRPG/Assets/CopyRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotation : MonoBehaviour {
+
+    Transform target;
+
+    public bool x;
+    public bool y;
+    public bool z;
+
+    private Vector3 newRotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        newRotation = transform.rotation;
+
+    }
+}
